a64c8fc5930c4425dbccb63e38727ef19273c0ce
SamBPP <112477701+SamBPP@users.noreply.github.com>
2025-10-09 15:47:07 +0000

    fix: Update PID file and log processing stats; modify script to clear output directory

--- input/mv_processed_to_ingest.sh
+++ input/mv_processed_to_ingest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curr_dir="/workspaces/L5-DE-Production-Environments-T1"
-rm $curr_dir/output/*.csv.gz
-mv $input_dir/input/processed/* $input_dir/input/ingestion/
+rm $curr_dir/output/*
+mv $curr_dir/input/processed/* $curr_dir/input/ingestion/
